cabd68528d08f59da206603a7e5271bc3325d97c
Akash Gayakwad <akashgayakwad123@gmail.com>
2019-02-27 04:10:48 +0530

    gesture animation added

--- App.js
+++ App.js
@@ -48,6 +48,6 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'skyblue',
+    backgroundColor: '#fff',
   },
 });
--- src/Deck.js
+++ src/Deck.js
@@ -1,10 +1,47 @@
 import React, {Component} from 'react';
-import {View, Text, Animated} from 'react-native';
+import {View, Text, Animated, PanResponder} from 'react-native';
 
 class Deck extends Component {
 
+  constructor(props){
+    super(props);
+    const position = new Animated.ValueXY();
+
+    const panResponder = PanResponder.create({
+      onStartShouldSetPanResponder: () => true,
+      onPanResponderMove: (event, gesture) => {
+        position.setValue({x:gesture.dx ,y:gesture.dy});
+        console.log(this.position);
+      },
+      onPanResponderRelease: () => {}
+    });
+    this.state = { panResponder, position };
+  }
+
+  getCardStyle(){
+    const {position} = this.state;
+    const rotate = position.x.interpolate({
+      inputRange: [-500, 0, 500],
+      outputRange: ['-120deg', '0deg', '120deg']
+    });
+
+   return{
+     ...this.state.position.getLayout(),
+     transform: [{ rotate: rotate }]
+   };
+  }
+
+
   renderCards(){
-    return this.props.data.map((item)=>{
+    return this.props.data.map((item, index)=>{
+      if(index == 0){
+        return(
+          <Animated.View key={item.id} style={this.getCardStyle()}
+          {...this.state.panResponder.panHandlers}>
+            {this.props.renderCard(item)}
+          </Animated.View>
+        )
+      }
       return this.props.renderCard(item);
     });
   }
